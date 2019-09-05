7bc5040f8d5d72e72d5b73472ec813ead18b7c94
anpeixu <496939565@qq.com>
2019-09-05 22:16:59 +0800

    提交

--- ly-common2/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common2/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -10,7 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ExceptionEnum {
    PRICE_CANNOT_BE_NULL(400,"价格不能为空！"),
-   CATEGORY_NOT_FOND(400,"商品分类没查到！");
+   CATEGORY_NOT_FOND(400,"商品分类没查到！"),
+   BRAND_NOT_FOND(404,"品牌没有查询到！");
    ;
    private int code;
    private String msg;

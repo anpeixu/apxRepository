4f9d458ad024eea511bd29efd836fc74484313c4
anpeixu <496939565@qq.com>
2019-09-06 19:37:30 +0800

    新增品牌

--- ly-common2/src/main/java/com/leyou/common/enums/ExceptionEnum.java
+++ ly-common2/src/main/java/com/leyou/common/enums/ExceptionEnum.java
@@ -9,9 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public enum ExceptionEnum {
-   PRICE_CANNOT_BE_NULL(400,"价格不能为空！"),
-   CATEGORY_NOT_FOND(400,"商品分类没查到！"),
-   BRAND_NOT_FOND(404,"品牌没有查询到！");
+    PRICE_CANNOT_BE_NULL(400,"价格不能为空！"),
+    CATEGORY_NOT_FOND(400,"商品分类没查到！"),
+    BRAND_NOT_FOND(404,"品牌没有查询到！"),
+    BRAND_SAVE_ERROR(500,"新增品牌失败 ！");
    ;
    private int code;
    private String msg;
